f94c5db6baddf67d8573c08648b92df18bbaf564
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2023-02-09 17:21:51 +0100

    Slide: prevent errors caused by empty slides.

--- src/slide/include/render_slide.php
+++ src/slide/include/render_slide.php
@@ -5,9 +5,11 @@
 function apblk_slide_render_callback( $attr ) {
 	ob_start();
 	?>
-		<div <?php echo get_block_wrapper_attributes( array( 'class' => 'swiper-slide' ) ) ?>>
-			<img class="object-cover w-full h-full" src="<?php echo $attr['imageUrl'] ?>" alt="<?php echo $attr['imageAlt'] ?>">
-		</div>
+		<?php if ( isset( $attr['imageUrl'] ) && ! empty( $attr['imageUrl'] ) ) : ?>
+			<div <?php echo get_block_wrapper_attributes( array( 'class' => 'swiper-slide' ) ) ?>>
+				<img class="object-cover w-full h-full" src="<?php echo $attr['imageUrl'] ?>" alt="<?php echo $attr['imageAlt'] ?>">
+			</div>
+		<?php endif; ?>
 	<?php
 	return ob_get_clean();
 }
